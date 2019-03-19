271f24ca0dfdb28a9973f0e94cb7f05a0b78d6a0
Andrew Waller <andrewhwaller@gmail.com>
2019-03-19 03:59:38 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,10 +1,10 @@
 require 'pry'
 
 def my_all?(collection)
-i = 0
-altered_array = []
-while i < collection.length
-    altered_array << yield(collection[i])
-    i = i + 1
-  end
+  i = 0
+  altered_array = []
+    while i < collection.length
+      altered_array << yield(collection[i])
+      i = i + 1
+    end
 end
